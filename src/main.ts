2a441a8546ba6111e8181847ca2767984965249f
ilDug <dognini.marco@gmail.com>
2022-11-04 17:50:30 +0100

    first implementation

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,53 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    try {
+        let release: string;
+        let tag: string;
+        let version: string;
+        let major: string;
+        let minor: string;
+        let patch: string;
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+        const ref = github.context.ref
+        const prefix = 'refs/tags/'
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+        if (!ref)
+            throw "REF not defined"
+        if (!ref.startsWith(prefix))
+            throw `Not a tag ref (${ref})`
+
+        release = ref.replace(/^refs\/tags\//, "")
+        const tagRegex = new RegExp(/v?([\d*][\.\d*]*)/i)
+        tag = release.match(tagRegex)![0] ?? ""
+        tag = tag.toLowerCase();
+        version = tag.startsWith("v") ? tag.replace(/^v/, "") : tag
+        const parts = version.split('.')
+        major = parts[0]
+        minor = parts[1] ?? ""
+        patch = parts[2] ?? ""
+
+
+        core.exportVariable('DAG_RELEASE', release)
+        core.setOutput('release', release)
+
+        core.exportVariable('DAG_TAG', tag)
+        core.setOutput('tag', tag)
+
+        core.exportVariable('DAG_MAJOR', major)
+        core.setOutput('major', major)
+
+        core.exportVariable('DAG_MINOR', minor)
+        core.setOutput('minor', minor)
+
+        core.exportVariable('DAG_PATCH', patch)
+        core.setOutput('patch', patch)
+
+
+    } catch (error) {
+        if (error instanceof Error) core.setFailed(error.message)
+    }
 }
 
 run()
